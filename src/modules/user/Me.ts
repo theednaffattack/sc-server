98fb2982a634a8fdff7bdb5f23fa1a2ec7bf8bb5
eddie naff <eddienaff@eddienaff.com>
2019-12-13 17:50:25 -0800

    Add logging and auth middleware to Me

--- src/modules/user/Me.ts
+++ src/modules/user/Me.ts
@@ -3,10 +3,11 @@ import { Resolver, Query, Ctx, UseMiddleware } from "type-graphql";
 import { User } from "../../entity/User";
 import { MyContext } from "../../types/MyContext";
 import { loggerMiddleware } from "../middleware/logger";
+import { isAuth } from "../middleware/isAuth";
 
 @Resolver()
 export class MeResolver {
-  @UseMiddleware(loggerMiddleware)
+  @UseMiddleware(isAuth, loggerMiddleware)
   @Query(() => User, { nullable: true })
   async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
     // if we can't find a userId on the current session
