b6af6995dcd6f7cc6a055f66893524edb6d1122e
Eddie Naff <eddie@eddienaff.dev>
2020-11-13 08:36:19 -0800

    Update config for new environment

--- src/global-utils/redis-config.ts
+++ src/global-utils/redis-config.ts
@@ -2,16 +2,16 @@ import { RedisPubSub } from "graphql-redis-subscriptions";
 import Redis from "ioredis";
 
 const options = {
-  host: process.env.REDIS_DOMAIN_NAME,
-  port: parseInt(process.env.REDIS_PORT_NUMBER!),
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT_NUMBER!, 10),
   retryStrategy: (times: any) => {
     // reconnect after
     return Math.min(times * 50, 2000);
-  }
+  },
 };
 
 export const pubsub = new RedisPubSub({
   // ...,
   publisher: new Redis(options),
-  subscriber: new Redis(options)
+  subscriber: new Redis(options),
 });
