a3e0e630301a2c5bfa2e1ff23f4f21a9309e9f65
Eddie Naff <eddie@eddienaff.dev>
2020-11-18 12:26:47 -0800

    Check ctx.userId for auth purposes

--- src/modules/middleware/isAuth.ts
+++ src/modules/middleware/isAuth.ts
@@ -3,7 +3,7 @@ import { MiddlewareFn } from "type-graphql";
 import { MyContext } from "../../types/MyContext";
 
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
-  if (!context.req.session!.userId) {
+  if (!context.req.session!.userId && !context.userId) {
     throw new Error("Not authenticated");
   }
 
