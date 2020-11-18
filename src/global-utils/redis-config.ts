39f034a33beb0218ceea58ae5c3e419b52afc361
Eddie Naff <eddie@eddienaff.dev>
2020-11-18 12:25:52 -0800

    Split redis configs based on env vars

--- src/global-utils/redis-config.ts
+++ src/global-utils/redis-config.ts
@@ -1,7 +1,16 @@
 import { RedisPubSub } from "graphql-redis-subscriptions";
 import Redis from "ioredis";
 
-const options = {
+const developmentOptions = {
+  host: process.env.REDIS_DEV_HOST,
+  port: parseInt(process.env.REDIS_PORT_NUMBER!, 10),
+  retryStrategy: (times: any) => {
+    // reconnect after
+    return Math.min(times * 50, 2000);
+  },
+};
+
+const productionOptions = {
   host: process.env.REDIS_HOST,
   port: parseInt(process.env.REDIS_PORT_NUMBER!, 10),
   retryStrategy: (times: any) => {
@@ -12,6 +21,12 @@ const options = {
 
 export const pubsub = new RedisPubSub({
   // ...,
-  publisher: new Redis(options),
-  subscriber: new Redis(options),
+  publisher:
+    process.env.NODE_ENV == "production"
+      ? new Redis(productionOptions)
+      : new Redis(developmentOptions),
+  subscriber:
+    process.env.NODE_ENV == "production"
+      ? new Redis(productionOptions)
+      : new Redis(developmentOptions),
 });
