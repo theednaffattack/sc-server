e0f5ee595f2f3a3a7ccbb0625820b0287bee9b5a
Eddie Naff <eddie@eddienaff.dev>
2020-12-13 10:46:19 -0800

    Create Thread payload type

---
+++ src/modules/channel/add-thread-payload.ts
@@ -0,0 +1,25 @@
+import { ObjectType, Field, ID } from "type-graphql";
+
+import { Message } from "../../entity/Message";
+import { User } from "../../entity/User";
+
+@ObjectType()
+export class AddThreadPayload {
+  @Field(() => Boolean)
+  success: boolean;
+
+  @Field(() => ID)
+  channelId: string;
+
+  @Field(() => ID)
+  threadId: string;
+
+  @Field(() => Message)
+  message: Message;
+
+  @Field(() => User)
+  sentBy: User;
+
+  @Field(() => [User], { nullable: "itemsAndList" })
+  invitees?: User[];
+}
