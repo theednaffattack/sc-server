56d29d2a7ee41f5b0373741a86d68752bbfd61a8
Eddie Naff <eddie@eddienaff.dev>
2020-12-28 15:45:11 -0800

    Adjust redis connections for TLS

--- src/redis.ts
+++ src/redis.ts
@@ -1,21 +1,46 @@
 import Redis from "ioredis";
+import path from "path";
+import fs from "fs";
+import { RedisPubSub } from "graphql-redis-subscriptions";
 
 const nodeEnvIs_NOT_Prod = process.env.NODE_ENV !== "production";
 
-const homeOptions: Redis.RedisOptions = {
+const developmentOptions: Redis.RedisOptions = {
   host: "localhost",
   port: 6379,
   retryStrategy: (times: any) => Math.max(times * 100, 3000),
+  showFriendlyErrorStack: true,
 };
 
-const prodOptions: Redis.RedisOptions = {
+const productionOptions: Redis.RedisOptions = {
   host: process.env.REDIS_HOST,
-  port: parseInt(process.env.REDIS_PORT_NUMBER!, 10),
+  port: parseInt(process.env.REDIS_INTERIOR_PORT!, 10),
   name: "myredis",
-  // password: process.env.REDIS_PASSWORD!,
+  password: process.env.REDIS_PASS!,
   retryStrategy: (times: any) => Math.max(times * 100, 3000),
+  showFriendlyErrorStack: true,
+  tls: {
+    ca: fs.readFileSync(path.resolve(__dirname, `../certs/fullchain.pem`)),
+    cert: fs.readFileSync(path.resolve(__dirname, `../certs/cert.pem`)),
+    key: fs.readFileSync(path.resolve(__dirname, `../certs/key.pem`)),
+    host: process.env.VIRTUAL_HOST,
+  },
 };
-
+export function redisError(error: Error) {
+  console.warn("error", error);
+}
 export const redis = nodeEnvIs_NOT_Prod
-  ? new Redis(homeOptions)
-  : new Redis(prodOptions);
+  ? new Redis(developmentOptions)
+  : new Redis(productionOptions);
+
+export const pubsub = new RedisPubSub({
+  // ...,
+  publisher:
+    process.env.NODE_ENV == "production"
+      ? new Redis(productionOptions)
+      : new Redis(developmentOptions),
+  subscriber:
+    process.env.NODE_ENV == "production"
+      ? new Redis(productionOptions)
+      : new Redis(developmentOptions),
+});
