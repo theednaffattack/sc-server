fcae96b5338557fa33a73fd15b72467b9606897b
Eddie Naff <eddie@eddienaff.dev>
2020-12-29 14:47:44 -0800

    Improve redis connection config

--- src/redis.ts
+++ src/redis.ts
@@ -24,25 +24,41 @@ const productionOptions: Redis.RedisOptions = {
     cert: fs.readFileSync(path.resolve(__dirname, `../certs/cert.pem`)),
     key: fs.readFileSync(path.resolve(__dirname, `../certs/key.pem`)),
     host: process.env.VIRTUAL_HOST,
+    secureProtocol: "TLSv1_2_method",
+    checkServerIdentity: () => {
+      return undefined;
+    },
   },
 };
 export function redisError(error: Error) {
-  console.warn("redis error", error);
+  console.warn("redis error", {
+    error,
+    productionOptions,
+    env: process.env.NODE_ENV,
+    isProd: nodeEnvIs_NOT_Prod,
+  });
 }
+
+export function redisReady(readyEvent: any) {
+  console.log("redis is ready", readyEvent);
+}
+
 export const redis = nodeEnvIs_NOT_Prod
   ? new Redis(developmentOptions)
   : new Redis(productionOptions);
 
 redis.on("error", redisError);
 
+redis.on("ready", redisReady);
+
 export const pubsub = new RedisPubSub({
   // ...,
   publisher:
-    process.env.NODE_ENV == "production"
+    process.env.NODE_ENV === "production"
       ? new Redis(productionOptions)
       : new Redis(developmentOptions),
   subscriber:
-    process.env.NODE_ENV == "production"
+    process.env.NODE_ENV === "production"
       ? new Redis(productionOptions)
       : new Redis(developmentOptions),
 });
