415ebac8cb3379fdd5a838c32f92aabae1e7df58
Eddie Naff <eddie@eddienaff.dev>
2021-03-02 16:20:59 -0800

    Add custom Directives to resolver

--- src/modules/user/edit-user-info.ts
+++ src/modules/user/edit-user-info.ts
@@ -1,26 +1,30 @@
-import { Arg, Resolver, Mutation, UseMiddleware, Ctx } from "type-graphql";
+import {
+  Arg,
+  Directive,
+  Resolver,
+  Mutation,
+  UseMiddleware,
+  Ctx,
+} from "type-graphql";
 
 import { User } from "../../entity/User";
 import { EditUserInput } from "./register/edit-user-input";
 import { isAuth } from "../middleware/isAuth";
 import { loggerMiddleware } from "../middleware/logger";
 import { MyContext } from "../../types/MyContext";
+import { TeamRoleEnum } from "../../entity/Role";
 
 let errorMessageBase = "Error saving info to database";
 
 @Resolver()
 export class EditUserInfoResolver {
   @UseMiddleware(isAuth, loggerMiddleware)
+  @Directive(`@hasRole(role: ${TeamRoleEnum.ADMIN})`)
+  @Directive(`@hasRole(role: ${TeamRoleEnum.OWNER})`)
   @Mutation(() => User)
   async editUserInfo(
     @Arg("data")
-    {
-      email,
-      firstName,
-      lastName,
-      // @ts-ignore
-      teamRoles
-    }: EditUserInput,
+    { email, firstName, lastName }: EditUserInput,
     @Ctx() ctx: MyContext
   ): Promise<any> {
     // most efficient way to set records in the DB
@@ -28,7 +32,7 @@ export class EditUserInfoResolver {
     let setObject = {
       email,
       firstName,
-      lastName
+      lastName,
     };
 
     await User.createQueryBuilder()
@@ -36,8 +40,8 @@ export class EditUserInfoResolver {
       .set(setObject)
       .where("id = :id", { id: ctx.userId })
       .execute()
-      .then(data => console.log("TYPICAL UPDATE", data))
-      .catch(error => console.error(error));
+      .then((data) => console.log("TYPICAL UPDATE", data))
+      .catch((error) => console.error(error));
 
     // since an error is thrown above on errors
     // it may be smarter to return the data passed in via
@@ -46,7 +50,7 @@ export class EditUserInfoResolver {
       .leftJoinAndSelect("user.teamRoles", "teamRoles")
       .where("user.id = :id", { id: ctx.userId })
       .getOne()
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
         throw Error(`${errorMessageBase}\n${error}`);
       });
