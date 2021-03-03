a044af6a949cb8f9cae5caecf8dfd47ada7f6545
Eddie Naff <eddie@eddienaff.dev>
2021-03-02 16:19:50 -0800

    Add access token to login response

--- src/modules/team/login-response.ts
+++ src/modules/team/login-response.ts
@@ -5,9 +5,12 @@ import { User } from "../../entity/User";
 
 @ObjectType()
 export class LoginResponse {
-  @Field(() => FieldError, { nullable: true })
+  @Field(() => [FieldError], { nullable: true })
   errors?: FieldError[];
 
   @Field(() => User, { nullable: true })
   user?: User;
+
+  @Field(() => String, { nullable: true })
+  accessToken?: string;
 }
