4aa6fd18342af84d8e3a73720454d62d151f0ad1
Eddie Naff <eddie@eddienaff.dev>
2021-03-02 16:21:18 -0800

    Add basic error handling

--- src/modules/user/ForgotPassword.ts
+++ src/modules/user/ForgotPassword.ts
@@ -1,5 +1,6 @@
 import { Resolver, Mutation, Arg } from "type-graphql";
 import { v4 } from "uuid";
+import internalIp from "internal-ip";
 
 import { redis } from "../../redis";
 import { User } from "../../entity/User";
@@ -17,12 +18,41 @@ export class ForgotPasswordResolver {
     }
 
     const token = v4();
-    await redis.set(forgotPasswordPrefix + token, user.id, "ex", 60 * 60 * 24); // 1 day expiration
 
-    await sendEmail(
-      email,
-      `http://localhost:3000/user/change-password/${token}`
-    );
+    const homeIp = internalIp.v4.sync();
+
+    const nodeEnvIsProd = process.env.NODE_ENV === "production";
+
+    try {
+      await redis.set(
+        forgotPasswordPrefix + token,
+        user.id,
+        "ex",
+        60 * 60 * 24
+      ); // 1 day expiration
+    } catch (error) {
+      console.error("redis token error", error);
+    }
+
+    if (nodeEnvIsProd) {
+      try {
+        await sendEmail(
+          email,
+          `https://${process.env.FRONT_END_PROTO_AND_DOMAIN_PROD}:${process.env.INTERNAL_API_PORT}/change-password/${token}`
+        );
+      } catch (error) {
+        console.error("Forgot password - send email error", error);
+      }
+    } else {
+      try {
+        await sendEmail(
+          email,
+          `http://${homeIp}:${process.env.INTERNAL_API_PORT}/change-password/${token}`
+        );
+      } catch (error) {
+        console.error("Forgot password - send email error", error);
+      }
+    }
 
     return true;
   }
