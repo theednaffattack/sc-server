a3bc9628903f8a5f1c7cdd17462b5f25db5a31a7
Eddie Naff <eddie@eddienaff.dev>
2021-03-25 11:52:41 -0700

    Fix MyContext types

--- src/types/MyContext.ts
+++ src/types/MyContext.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { GraphQLResolveInfo, GraphQLArgs } from "graphql";
+import { UserToTeam } from "../entity/UserToTeam";
 import { User } from "../entity/User";
 // import DataLoader = require("dataloader");
 
@@ -8,12 +9,19 @@ interface GraphQlInputs {
   info: GraphQLResolveInfo;
 }
 
+interface TokenPayload {
+  userId: string;
+  iat: number;
+  exp: number;
+}
+
 export interface MyContext {
   userId: User["id"];
+  userTeam?: UserToTeam;
   gqlOpts: GraphQlInputs;
   req: Request;
   res: Response;
   next: NextFunction;
   usersLoader?: any;
-  payload?: { userId: string };
+  payload?: { token?: TokenPayload; errors?: any[] };
 }
