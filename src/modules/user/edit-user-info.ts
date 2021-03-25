37b7cc5f59c83b539e5c422320c93333c54774eb
Eddie Naff <eddie@eddienaff.dev>
2021-03-25 11:49:31 -0700

    Remove directives

--- src/modules/user/edit-user-info.ts
+++ src/modules/user/edit-user-info.ts
@@ -1,32 +1,52 @@
 import {
   Arg,
-  Directive,
-  Resolver,
-  Mutation,
-  UseMiddleware,
+  Authorized,
   Ctx,
+  Field,
+  Mutation,
+  ObjectType,
+  Resolver,
+  UseMiddleware,
 } from "type-graphql";
-
+import { TeamRoleEnum } from "../../entity/Role";
 import { User } from "../../entity/User";
-import { EditUserInput } from "./register/edit-user-input";
+import { UserToTeam } from "../../entity/UserToTeam";
+import { FieldError } from "../../lib/gql-type.field-error";
+import { MyContext } from "../../types/MyContext";
 import { isAuth } from "../middleware/isAuth";
 import { loggerMiddleware } from "../middleware/logger";
-import { MyContext } from "../../types/MyContext";
-import { TeamRoleEnum } from "../../entity/Role";
+import { EditUserInput } from "./register/edit-user-input";
 
 let errorMessageBase = "Error saving info to database";
 
+@ObjectType()
+class UserExtendedWithTeamRoles {
+  @Field(() => User, { nullable: false })
+  user: User;
+
+  @Field(() => [TeamRoleEnum], { nullable: false })
+  teamRoles: TeamRoleEnum[];
+}
+
+@ObjectType()
+export class EditUserInfoResponse {
+  @Field(() => FieldError, { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => UserExtendedWithTeamRoles, { nullable: true })
+  userWithRoles?: UserExtendedWithTeamRoles;
+}
+
 @Resolver()
-export class EditUserInfoResolver {
+export class EditCurrentUserInfoResolver {
   @UseMiddleware(isAuth, loggerMiddleware)
-  @Directive(`@hasRole(role: ${TeamRoleEnum.ADMIN})`)
-  @Directive(`@hasRole(role: ${TeamRoleEnum.OWNER})`)
-  @Mutation(() => User)
-  async editUserInfo(
+  @Authorized("ADMIN", "OWNER")
+  @Mutation(() => EditUserInfoResponse)
+  async editCurrentUserInfo(
     @Arg("data")
-    { email, firstName, lastName }: EditUserInput,
+    { email, firstName, lastName, teamRoles }: EditUserInput,
     @Ctx() ctx: MyContext
-  ): Promise<any> {
+  ): Promise<EditUserInfoResponse> {
     // most efficient way to set records in the DB
     // returns nothing
     let setObject = {
@@ -38,23 +58,74 @@ export class EditUserInfoResolver {
     await User.createQueryBuilder()
       .update(User)
       .set(setObject)
-      .where("id = :id", { id: ctx.userId })
+      .where("id = :id", { id: ctx.payload?.token?.userId })
       .execute()
       .then((data) => console.log("TYPICAL UPDATE", data))
       .catch((error) => console.error(error));
 
-    // since an error is thrown above on errors
-    // it may be smarter to return the data passed in via
-    // arguments rather than perform a lookup
-    let userToReturn = await User.createQueryBuilder("user")
-      .leftJoinAndSelect("user.teamRoles", "teamRoles")
-      .where("user.id = :id", { id: ctx.userId })
+    let userToCheckAttributes = await UserToTeam.createQueryBuilder("utt")
+      .where("utt.userId = :id", { id: ctx.payload?.token?.userId })
       .getOne()
       .catch((error) => {
         console.error(error);
         throw Error(`${errorMessageBase}\n${error}`);
       });
 
-    return userToReturn;
+    // ENUM TO ARRAY
+    // adapted from: https://github.com/microsoft/TypeScript/issues/31268#issuecomment-489660702
+    const realTeamRoles = userToCheckAttributes?.teamRoleAuthorizations.includes(
+      TeamRoleEnum.OWNER
+    )
+      ? [
+          ...Object.values(teamRoles!).filter((k) => typeof k === "string"),
+          TeamRoleEnum.OWNER,
+        ]
+      : teamRoles;
+
+    await UserToTeam.createQueryBuilder()
+      .update(UserToTeam)
+      .where({ userId: userToCheckAttributes?.userId })
+      .set({
+        teamRoleAuthorizations: realTeamRoles,
+      })
+      .execute()
+      .then((data) => {
+        console.log("UPDATE USER TO TEAM MERGE TABLE", {
+          data,
+          userId: ctx.payload?.token?.userId,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    // since an error is thrown above on errors
+    // it may be smarter to return the data passed in via
+    // arguments rather than perform a lookup
+    let userToReturn = await User.createQueryBuilder("user")
+      .where("user.id = :id", { id: ctx.payload?.token?.userId })
+      .getOne()
+      .catch((error) => {
+        console.error(error);
+        throw Error(`${errorMessageBase}\n${error}`);
+      });
+
+    if (!userToReturn) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "There was an error updating your info.",
+          },
+        ],
+      };
+    }
+
+    return {
+      userWithRoles: {
+        teamRoles: realTeamRoles ?? [],
+        user: userToReturn,
+      },
+    };
   }
 }
