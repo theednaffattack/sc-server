9fe32d8224d8f04b4d05d55e984832b66049f552
Eddie Naff <eddie@eddienaff.dev>
2021-03-25 11:48:54 -0700

    Adjust me resolver to investigate errors client side

--- src/modules/user/Me.ts
+++ src/modules/user/Me.ts
@@ -4,17 +4,61 @@ import { User } from "../../entity/User";
 import { MyContext } from "../../types/MyContext";
 import { loggerMiddleware } from "../middleware/logger";
 import { isAuth } from "../middleware/isAuth";
+import { MeResponse } from "./me/me-response";
 
 @Resolver()
 export class MeResolver {
   @UseMiddleware(isAuth, loggerMiddleware)
-  @Query(() => User, { nullable: true })
-  async me(@Ctx() ctx: MyContext): Promise<User | undefined> {
-    // if we can't find a userId on the current session
-    if (!ctx.req.session!.userId) {
-      return undefined;
+  @Query(() => MeResponse, { nullable: true })
+  async me(@Ctx() ctx: MyContext): Promise<MeResponse> {
+    // if we can't find a payload or userId on the current context
+
+    if (ctx.payload?.errors && ctx.payload.errors.length > 0) {
+      return {
+        errors: [
+          {
+            field: "general-user-facing",
+            message: `Auth error - ${ctx.payload.errors[0].message}`,
+          },
+        ],
+      };
     }
 
-    return User.findOne(ctx.req.session!.userId);
+    if (!ctx.payload?.token?.userId) {
+      return {
+        errors: [
+          {
+            field: "general-user-facing",
+            message: `Auth error - not authenticated`,
+          },
+        ],
+      };
+    }
+
+    let user;
+    try {
+      user = await User.findOne(ctx.payload.token?.userId);
+    } catch (error) {
+      return {
+        errors: [
+          {
+            field: "general-user-facing",
+            message: `Error fetching user - ${error.message}`,
+          },
+        ],
+      };
+    }
+    if (user) {
+      return { user };
+    } else {
+      return {
+        errors: [
+          {
+            field: "general-user-facing-error",
+            message: "Unexpected error",
+          },
+        ],
+      };
+    }
   }
 }
