0c080749730a4f60f9a3389a2c085af79ca0c6fd
Eddie Naff <eddie@eddienaff.dev>
2021-04-29 09:09:47 -0700

    Refactor for different environments

--- src/modules/utils/createConfirmationUrl.ts
+++ src/modules/utils/createConfirmationUrl.ts
@@ -1,14 +1,46 @@
-// import { v4 } from "uuid";
+import internalIp from "internal-ip";
 import { redis } from "../../redis";
 import { confirmUserPrefix } from "../constants/redisPrefixes";
 
-export const createConfirmationUrl = async (userId: string) => {
-  // const token = v4();
-  await redis.set(confirmUserPrefix + userId, userId, "ex", 60 * 60 * 24); // 1 day expiration
+export const createConfirmationUrl = async (
+  userId: string
+): Promise<string> => {
+  const nodeEnv = process.env.NODE_ENV;
 
-  return `${
-    process.env.NODE_ENV === "production"
-      ? process.env.FRONT_END_PROTO_AND_DOMAIN_PROD
-      : process.env.FRONT_END_PROTO_AND_DOMAIN_DEV
-  }/confirmation/${userId}`;
+  if (process.env.TEST_CLIENT_ORIGIN && nodeEnv === "test") {
+    const client = process.env.TEST_CLIENT_ORIGIN;
+    try {
+      await redis.set(confirmUserPrefix + userId, userId, "ex", 60 * 60 * 24); // 1 day expiration
+    } catch (error) {
+      throw Error(error);
+    }
+    return `${client}/user/confirm/${userId}`;
+  }
+
+  if (
+    process.env.DEVELOPMENT_CLIENT_ORIGIN &&
+    process.env.INTERNAL_API_PORT &&
+    nodeEnv === "development"
+  ) {
+    const port = process.env.INTERNAL_API_PORT;
+    const client = internalIp.v4() + ":" + port;
+    try {
+      await redis.set(confirmUserPrefix + userId, userId, "ex", 60 * 60 * 24); // 1 day expiration
+    } catch (error) {
+      throw Error(error);
+    }
+    return `${client}/user/confirm/${userId}`;
+  }
+
+  if (process.env.PRODUCTION_CLIENT_ORIGIN && nodeEnv === "production") {
+    const client = process.env.PRODUCTION_CLIENT_ORIGIN;
+    try {
+      await redis.set(confirmUserPrefix + userId, userId, "ex", 60 * 60 * 24); // 1 day expiration
+    } catch (error) {
+      throw Error(error);
+    }
+    return `${client}/user/confirm/${userId}`;
+  }
+
+  throw Error("Cannot detect confirmation URL domain.");
 };
